bc3b9436e55bc2c570249df4f2c59c67d5db93c0
Čeněk Použník <potuznikcenek@gmail.com>
2024-06-02 10:14:42 +0200

    added student class into db

--- app/db_classes.py
+++ app/db_classes.py
@@ -9,3 +9,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=False, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    code = db.Column(db.Integer, unique=True, nullable=False)
+    cip = db.Column(db.String(10), unique=True)

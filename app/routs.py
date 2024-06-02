abccf6e321945d3543beedc0d0900f0442358325
Čeněk Použník <potuznikcenek@gmail.com>
2024-06-02 14:41:12 +0200

    added editing students

--- app/routs.py
+++ app/routs.py
@@ -25,15 +25,20 @@ def edit_class(class_name):
     students = [student for student in Student.query.all() if class_name_from_code(student.code) == class_name]
     return render_template('edit/edit_class.html', students=students, classes=CLASSES, class_name=class_name)
 
-@app.post('/edit/student/<student_id>/<new_rfid>')
+@app.route('/edit/student/<student_id>', methods=['GET', 'POST'])
 @login_required
-def edit_student_rfid(student_id, new_rfid):
+def edit_student(student_id):
     if not student_id.isdigit(): abort(500)
     student = Student.query.get(student_id)
     if not student: abort(500)
-    if len(new_rfid) != 10: abort(500)
-    student.rfid = new_rfid
-    db.session.commit()
+    if request.method == 'POST':
+        rfid = request.form["rfid"]
+        if rfid:
+            student.rfid = rfid
+            db.session.commit()
+    class_name = class_name_from_code(student.code)
+    class_students = [student_ for student_ in Student.query.all() if class_name_from_code(student_.code) == class_name] # vsichni zaci z tridy daneho zaka
+    return render_template('edit/edit_student.html', student=student, class_name=class_name, classes=CLASSES, students=class_students)
 
 @app.post('/add') # post request na pridani studenta, pro ucely migrace ze starsi databaze. POZOR - je potreba nezapomenout zabezpecit (@login_required) !!
 @login_required
